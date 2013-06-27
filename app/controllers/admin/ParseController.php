0efb239dd39e68a4d0d07359565b2931b7c7e3b7
IvanBohun <drivanbohun@gmail.com>
2013-06-27 12:45:43 -0500

    duka

---
+++ app/controllers/admin/ParseController.php
@@ -0,0 +1,30 @@
+<?php
+//LOCATION: remax/public/parse 
+
+use Sunra\PhpSimple\HtmlDomParser;
+
+class ParseController extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
+	*/
+
+	public function index()
+	{
+	$html = readfile( 'http://www.remax-ni.net/realestatehomesforsale/60076-p001.html' );
+	echo $html;
+	      
+
+	return 'test';
+	}
+
+}
