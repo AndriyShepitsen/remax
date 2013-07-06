bdc0d8df5f794403bc8941e9de1187cd9b25a26d
IvanBohun <drivanbohun@gmail.com>
2013-07-06 16:39:44 -0500

    SD

--- app/controllers/search/SearchController.php
+++ app/controllers/search/SearchController.php
@@ -17,11 +17,34 @@ class SearchController extends BaseController {
 
     }
 
-  public function find()
+     public function find()
     {
-    $houses = House::with('images')->get();
+    $houses = "House::with('images')";
+    
 
-    return View::make('search.results')->with(compact('houses'));
+    $location  = Input::get('location');
+    if($location)
+    $houses .="->where('address', 'LIKE', '%'.$location.'%')";
+
+     $price_l  = Input::get('from');
+    if($price_l)
+    $houses .="->where('price', '>=', $price_l)";
+
+    $price_h  = Input::get('to');
+    if($price_h)
+    $houses .="->where('price', '<=', $price_h)";
+
+    $beds  = Input::get('beds');
+    if($beds)
+    $houses .="->where('bedrooms', '>=', $beds)";
+    
+    $baths  = Input::get('baths');
+    if($baths)
+    $houses .="->where('bathrooms', '>=', $baths)";
+
+   // $houses = ${houses};
+    dd($houses);
+   // return View::make('search.results')->with(compact('houses'));
 
     }
     public function show($id)
