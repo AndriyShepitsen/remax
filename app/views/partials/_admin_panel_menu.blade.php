1385def68199c5e300f7719659fd170b3566a70f
Ivan Bohun <drivanbohun@gmail.com>
2013-08-02 08:20:09 -0500

    user panel grid system

--- app/views/partials/_admin_panel_menu.blade.php
+++ app/views/partials/_admin_panel_menu.blade.php
@@ -2,13 +2,17 @@
 	<div class="large-1 column userImgL">
 		<img class="userImg"src="{{url('comp/img/userL.png')}}" alt="name">
 	</div>
-	<div class="large-11 columns userPanel">
+	<div class="large-9 columns userPanel">
 		<h1>User Panel</h1>
+	</div>
+	<div class="large-2 columns"></div>
+	<div class="editProfile button tiny secondary">
+		{{link_to('user-edit/'.Auth::user()->id, 'Edit My Profile', array('class'=>'aEditProfileStyle'))}} 
 	</div>
 </div>          
 <hr/>
 <div class="row">
-	<div class="large-12 columns">
+<div class="large-12 columns requestButtons">
 		<ul class="inline-list ulUserPanel">	
 			<li class="leaveRequest button tiny">
 
@@ -17,9 +21,8 @@
 			<li class="manageRequest button tiny">	
 				{{link_to('dream-home-request-management', 'Manage My Previous Request', array('class'=>'aUserPanelStyle'))}} 
 			</li>
-			<li class="right editProfile button tiny secondary">
-				{{link_to('user-edit/'.Auth::user()->id, 'Edit My Profile', array('class'=>'aEditProfileStyle'))}} 
+			<li class="manageRequest button tiny">	
+				{{link_to('dream-home-request-management', 'My Saved Homes', array('class'=>'aUserPanelStyle'))}} 
 			</li>
 		</ul>
-	</div>
-</div>
+	</div></div>
