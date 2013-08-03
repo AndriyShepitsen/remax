d24812d5cac058f3249bbdd8c9dd52d16972dc56
Ivan Bohun <drivanbohun@gmail.com>
2013-08-03 11:00:14 -0500

    manage my previous request page disign

--- app/views/admin/vw_dream_home_manage.blade.php
+++ app/views/admin/vw_dream_home_manage.blade.php
@@ -8,25 +8,45 @@
 			@include('partials._admin_panel_menu')
 
 			<h4>Manage Dreamhomes Request</h4>
-			@if(isset($dreamhomes))
-			<ol>
-			@foreach($dreamhomes as $dreamhome)
-			<li>
-			<div class="panel radius">
-			<em> Location:</em>{{$dreamhome->location}} <br/>
-			<em> Property type:</em>{{$dreamhome->property_type}} <br/>
-			<em> # of beds:</em>{{$dreamhome->beds}} <br/>
-			<em> # of baths:</em>{{$dreamhome->baths}} <br/>
-			<em> Garage:</em>{{$dreamhome->garage}} <br/>
-			{{link_to('dream-home-request-edit/'.$dreamhome->id, 'Edit')}} ||
-			{{link_to('dream-home-request-delete/'.$dreamhome->id, 'Delete')}}
-			
-
+			<div class="row">
+				<div class="large-6 columns">
+					@if(isset($dreamhomes))
+					<ol class="olDreamhomes">
+						<li>
+							@foreach($dreamhomes as $dreamhome)
+							<ul class="no-bullet panel radius">
+								<li>
+									<em> Location: </em>{{$dreamhome->location}} <br/>
+								</li>
+								<li>
+									<em> Property type: </em>{{$dreamhome->property_type}} <br/>
+								</li>
+								<li>
+									<em> Beds: </em>{{$dreamhome->beds}} <br/>
+								</li>
+								<em> Baths: </em>{{$dreamhome->baths}} <br/>
+								<li>
+									<em> Garage: </em>{{$dreamhome->garage}} <br/>
+								</li>
+								<li>
+									<ul class="inline-list">
+										<li>
+											{{link_to('dream-home-request-edit/'.$dreamhome->id, 'Edit')}} 
+										</li>
+										<li>
+											{{link_to('dream-home-request-delete/'.$dreamhome->id, 'Delete')}}
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+						<li>
+							@endforeach
+						</li>
+					</ol>
+						@endif
+				</div>
 			</div>
-			</li>
-			</ol>
-			@endforeach
-			@endif
 
 		</div>
 	</div>
