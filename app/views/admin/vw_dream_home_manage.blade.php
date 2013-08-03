03ae3bb2168f1c304f4e2a7ca130007eb0e0cb77
Ivan Bohun <drivanbohun@gmail.com>
2013-08-03 14:19:17 -0500

    Delete works

--- app/views/admin/vw_dream_home_manage.blade.php
+++ app/views/admin/vw_dream_home_manage.blade.php
@@ -24,48 +24,51 @@
 				<br/>
 				<div class="row">
 					<div class="large-7 columns">
-						
+						@if(isset($dreamhomes))	
 
-						@foreach($dreamhomes as $dreamhome)
-						<li>
-							<ul class="no-bullet panel radius">
-								<li>
-									<em> Location: </em>{{$dreamhome->location}} <br/>
-								</li>
-								<li>
-									<em> Property type: </em>{{$dreamhome->property_type}} <br/>
-								</li>
-								<li>
-									<em> Beds: </em>{{$dreamhome->beds}} <br/>
-								</li>
-								<em> Baths: </em>{{$dreamhome->baths}} <br/>
-								<li>
-									<em> Garage: </em>{{$dreamhome->garage}} <br/>
-								</li>
-								<li>
-									<em> Comments: </em>{{$dreamhome->comments}} <br/>
-								</li>
+						<ol class="olDreamhomes">
+							@foreach($dreamhomes as $dreamhome)
+							<li>
+								<ul class="no-bullet panel radius">
+									<li>
+										<em> Location: </em>{{$dreamhome->location}} <br/>
+									</li>
+									<li>
+										<em> Property type: </em>{{$dreamhome->property_type}} <br/>
+									</li>
+									<li>
+										<em> Beds: </em>{{$dreamhome->beds}} <br/>
+									</li>
+									<em> Baths: </em>{{$dreamhome->baths}} <br/>
+									<li>
+										<em> Garage: </em>{{$dreamhome->garage}} <br/>
+									</li>
+									<li>
+										<em> Comments: </em>{{$dreamhome->comments}} <br/>
+									</li>
 
 
-								<li>
-									<ul class="inline-list">
-										<li>
-											{{link_to_route('dream-home-request-edit', 
-											'Edit', $dreamhome->id)}} 
-										</li>
-										<li>
-							    {{link_to_route('dream-home-request-delete', 
-							    'Delete', $dreamhome->id)}}
-										</li>
-									</ul>
-								</li>
-							</ul>
-						</li>
-						@endforeach
-					</ol>
-					@endif
+									<li>
+										<ul class="inline-list">
+											<li>
+												{{link_to_route('dream-home-request-edit', 
+												'Edit', $dreamhome->id)}} 
+											</li>
+											<li>
+													{{Form::open(array('url' => 'dreamhomes/'.$dreamhome->id.'/delete', 'method'=>'DELETE'))}}
+{{ Form::submit('Delete', array('class'=>'button small secondary radius'))}}
+													{{Form::close()}}
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							@endforeach
+						</ol>
+						@endif
+					</div>
+
 				</div>
-				
 			</div>
 		</div>
 	</div>
