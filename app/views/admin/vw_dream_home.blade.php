0ae04dfa7fc47da571a9ea7bc249d8fd2b3d8e9f
Ivan Bohun <drivanbohun@gmail.com>
2013-08-03 13:04:32 -0500

    leave a request for my dream home form gesign

--- app/views/admin/vw_dream_home.blade.php
+++ app/views/admin/vw_dream_home.blade.php
@@ -10,45 +10,43 @@
 			<div class="panel userPanelContent">
 				<div class="row">
 
-				<div class="large-6 large-centered columns">	
-				<h4> Dream Home Request</h4>
+				<div class="large-11 large-centered columns">	
+				<h4 class="subheader"> Your Dreamhome Request</h4>
+				<br/>
 				{{Form::open(array('route' => 'my-dream-home-request', 'method'=>'POST'))}}
 
 
 						<div class="row">
-							<div class="large-12 columns">
+							<div class="large-8 columns">
 								{{Form::label('location', 'Location')}}
 								{{Form::text('location','', array('placeholder'=>'City, State, Address or Zip Code', 'class'=>'findInput'))}}
+							</div>
+							<div class="large-4 columns">
+								{{Form::label('property_type', 'Property Type')}}
+								{{Form::text('property_type','', array('placeholder'=>'', 'class'=>'findInput'))}}
 							</div>
 						</div>
 
 						<div class="row">
-							<div class="large-6 columns">
-								{{Form::label('property_type', 'Property Type')}}
-								{{Form::text('property_type','', array('placeholder'=>'', 'class'=>'findInput'))}}
-							</div>
 							
-							<div class="large-6 columns">
+							<div class="large-4 columns">
+								{{Form::label('beds', 'Bedrooms')}}
+								{{Form::text('beds', '', array('placeholder'=>'#', 'class'=>'findinput'))}}
+							</div>
+							<div class="large-4 columns">
+								{{Form::label('baths', 'Bathrooms')}}
+								{{Form::text('baths', Input::get('baths'), array('placeholder'=>'#', 'class'=>'findinput'))}}
+							</div>
+							<div class="large-4 columns">
 								{{Form::label('garage', 'Garage')}}
 								{{Form::text('garage','', array('placeholder'=>'', 'class'=>'findInput'))}}
 
 							</div>
-						</div>
-
-						<div class="row">
-							<div class="large-6 columns">
-								{{Form::label('beds', 'Bedrooms')}}
-								{{Form::text('beds', '', array('placeholder'=>'#', 'class'=>'findinput'))}}
-							</div>
-							<div class="large-6 columns">
-								{{Form::label('baths', 'Bathrooms')}}
-								{{Form::text('baths', Input::get('baths'), array('placeholder'=>'#', 'class'=>'findinput'))}}
-							</div>
 
 						</div>
-						<div class="row">
-							<div class="large-12 columns">
-								{{Form::label('location', 'Comments')}}
+						<div class="row textareaCommentsWrapper">
+							<div class="large-8 columns">
+								{{Form::label('location', 'Your Comments')}}
 								{{Form::textarea('location','', array('placeholder'=>'', 'class'=>'findInput'))}}
 							</div>
 						</div>
--- app/views/admin/vw_dream_home_manage.blade.php
+++ app/views/admin/vw_dream_home_manage.blade.php
@@ -7,62 +7,64 @@
 		<div class="large-12 columns userPanel">
 			@include('partials._admin_panel_menu')
 
-			<h4 class="subheader">Manage Your Dreamhomes Requests</h4>
-
-			<div class="row">
-				<div class="large-12 columns">
-					@if(isset($note))
-					<div class="label radius success updated">
-						{{$note}}
+			<div class="panel userPanelContent">
+				<h4 class="subheader">Manage My Dreamhome Requests</h4>
+				<br/>	
+				<div class="row">
+					<div class="large-12 columns">
+						@if(isset($note))
+						<div class="label radius success updated">
+							{{$note}}
+						</div>
+						<hr/>
+						@endif
 					</div>
-					<hr/>
-					@endif
+					
 				</div>
-
-			</div>
-			<br/>
-			<div class="row">
-				<div class="large-7 columns">
-
-					@if(isset($dreamhomes))
-					<ol class="olDreamhomes">
+				<br/>
+				<div class="row">
+					<div class="large-7 columns">
 						
-						@foreach($dreamhomes as $dreamhome)
-						<li>
-							<ul class="no-bullet panel radius">
-								<li>
-									<em> Location: </em>{{$dreamhome->location}} <br/>
-								</li>
-								<li>
-									<em> Property type: </em>{{$dreamhome->property_type}} <br/>
-								</li>
-								<li>
-									<em> Beds: </em>{{$dreamhome->beds}} <br/>
-								</li>
-								<em> Baths: </em>{{$dreamhome->baths}} <br/>
-								<li>
-									<em> Garage: </em>{{$dreamhome->garage}} <br/>
-								</li>
-								<li>
-									<ul class="inline-list">
-										<li>
-											{{link_to_route('dream-home-request-edit', 
-											'Edit', $dreamhome->id)}} 
-										</li>
-										<li>
-							    {{link_to_route('dream-home-request-delete', 
-							    'Delete', $dreamhome->id)}}
-										</li>
-									</ul>
-								</li>
-							</ul>
-						</li>
-						@endforeach
-					</ol>
-					@endif
+						@if(isset($dreamhomes))
+						<ol class="olDreamhomes">
+							
+							@foreach($dreamhomes as $dreamhome)
+							<li>
+								<ul class="no-bullet panel radius">
+									<li>
+										<em> Location: </em>{{$dreamhome->location}} <br/>
+									</li>
+									<li>
+										<em> Property type: </em>{{$dreamhome->property_type}} <br/>
+									</li>
+									<li>
+										<em> Beds: </em>{{$dreamhome->beds}} <br/>
+									</li>
+									<em> Baths: </em>{{$dreamhome->baths}} <br/>
+									<li>
+										<em> Garage: </em>{{$dreamhome->garage}} <br/>
+									</li>
+									<li>
+										<ul class="inline-list">
+											<li>
+												{{link_to_route('dream-home-request-edit', 
+												'Edit', $dreamhome->id)}} 
+											</li>
+											<li>
+												{{link_to_route('dream-home-request-delete', 
+												'Delete', $dreamhome->id)}}
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							@endforeach
+						</ol>
+						@endif
+					</div>
 				</div>
+				
 			</div>
-
 		</div>
 	</div>
 </div>
